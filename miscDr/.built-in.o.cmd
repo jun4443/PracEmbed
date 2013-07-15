624e81cf2fd69ada7b49dafca47d2abe669de023
Black <black@black-desktop.(none)>
2013-07-15 10:03:18 +0900

    misc Driver add

---
+++ miscDr/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_/home/black/moduleP/miscDr/built-in.o :=  rm -f /home/black/moduleP/miscDr/built-in.o; ar rcs /home/black/moduleP/miscDr/built-in.o
---
+++ miscDr/.tmp_versions/preempt_test.mod
@@ -0,0 +1,2 @@
+/home/black/moduleP/miscDr/preempt_test.ko
+/home/black/moduleP/miscDr/preempt_test.o
